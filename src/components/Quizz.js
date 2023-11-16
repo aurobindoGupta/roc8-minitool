4b486e25b95f8b4977dbc6b3fad384261b3594eb
AurobindoGupta <aurogpt10@gmail.com>
2023-11-16 14:42:01 +0530

    added questions array

--- src/components/Quizz.js
+++ src/components/Quizz.js
@@ -1,7 +1,12 @@
 const Quizz = () => {
+    const questions = [
+      { que: "What is the difference between let, var, and const?" },
+      { que: "What is debouncing and why is it used?" },
+      { que: "Explain closures in JavaScript." },
+    ];
   return (
     <>
-      <div>question</div>
+      <div>{Object.values(questions[0])}</div>
       <div>ans</div>
     </>
   );
